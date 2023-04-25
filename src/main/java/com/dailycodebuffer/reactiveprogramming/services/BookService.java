17fd00d57ad805e2d2ff4b47d4c9872c4fad3a85
tYohannes <teklegy@gmail.com>
2023-04-25 22:07:17 +0300

    add basic getBookByID functionality

--- src/main/java/com/dailycodebuffer/reactiveprogramming/services/BookService.java
+++ src/main/java/com/dailycodebuffer/reactiveprogramming/services/BookService.java
@@ -1,8 +1,8 @@
 package com.dailycodebuffer.reactiveprogramming.services;
 
 import com.dailycodebuffer.reactiveprogramming.domain.Book;
+import com.dailycodebuffer.reactiveprogramming.domain.BookInfo;
 import com.dailycodebuffer.reactiveprogramming.domain.Review;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,8 +11,8 @@ import java.util.List;
 
 @Slf4j
 public class BookService {
-    private BookInfoService bookInfoService;
-    private ReviewService reviewService;
+    private final BookInfoService bookInfoService;
+    private final ReviewService reviewService;
 
     public BookService(BookInfoService bookInfoService, ReviewService reviewService) {
         this.bookInfoService = bookInfoService;
@@ -28,5 +28,12 @@ public class BookService {
                     return reviews.map(review -> new Book(bookInfo, review));
                 })
                 .log();
+    }
+
+    public Mono<Book> getBookById(long bookId) {
+        var bookInfo = bookInfoService.getBookById(bookId);
+        var reviews = reviewService.getReviews(bookId).collectList();
+
+        return bookInfo.zipWith(reviews, Book::new);
     }
 }
