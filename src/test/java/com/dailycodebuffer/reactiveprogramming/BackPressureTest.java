457a1734e3208d31d7097d796d134684fdd86309
tYohannes <teklegy@gmail.com>
2023-04-25 22:56:59 +0300

    use onBackPressureError operator

--- src/test/java/com/dailycodebuffer/reactiveprogramming/BackPressureTest.java
+++ src/test/java/com/dailycodebuffer/reactiveprogramming/BackPressureTest.java
@@ -110,4 +110,39 @@ public class BackPressureTest {
             }
         });
     }
+
+    @Test
+    public void testBackPressureError() {
+        var numbers = Flux.range(1, 100).log();
+
+        numbers
+                .onBackpressureError()
+                .subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(3);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("value = " + value);
+                if (value == 3) hookOnCancel();
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                System.out.println("Completed");
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                System.out.println("throwable = " + throwable);
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                super.hookOnCancel();
+            }
+        });
+    }
 }
