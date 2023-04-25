4247d05fafe2a86bc1b326c06871e7e6e0c20949
tYohannes <teklegy@gmail.com>
2023-04-25 21:56:27 +0300

    add basic domain and service classes

---
+++ src/main/java/com/dailycodebuffer/reactiveprogramming/services/BookService.java
@@ -0,0 +1,32 @@
+package com.dailycodebuffer.reactiveprogramming.services;
+
+import com.dailycodebuffer.reactiveprogramming.domain.Book;
+import com.dailycodebuffer.reactiveprogramming.domain.Review;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
+public class BookService {
+    private BookInfoService bookInfoService;
+    private ReviewService reviewService;
+
+    public BookService(BookInfoService bookInfoService, ReviewService reviewService) {
+        this.bookInfoService = bookInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Flux<Book> getBooks() {
+        var allBooks = bookInfoService.getBooks();
+
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReviews(bookInfo.getBookId()).collectList();
+                    return reviews.map(review -> new Book(bookInfo, review));
+                })
+                .log();
+    }
+}
