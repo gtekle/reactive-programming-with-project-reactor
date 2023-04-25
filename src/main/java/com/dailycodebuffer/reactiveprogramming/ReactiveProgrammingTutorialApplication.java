e74465a25bc4b7dfc0209f4ee5eaa97b3a565a04
tYohannes <teklegy@gmail.com>
2023-04-25 12:40:51 +0300

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/reactiveprogramming/ReactiveProgrammingTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.reactiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProgrammingTutorialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dailycodebuffer/reactiveprogramming/services/FluxAndMonoService.java
@@ -0,0 +1,24 @@
+package com.dailycodebuffer.reactiveprogramming.services;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class FluxAndMonoService {
+    public Flux<String> fruitsFlux() {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"));
+    }
+
+    public Mono<String> fruitMono() {
+        return Mono.just("Mango");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
+
+        fluxAndMonoService.fruitsFlux().subscribe(System.out::println);
+
+        fluxAndMonoService.fruitMono().subscribe(fruit -> System.out.println("Mono: fruit = " + fruit));
+    }
+}
